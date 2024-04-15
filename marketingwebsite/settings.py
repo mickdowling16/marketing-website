a5c56f14fdda77de9fffe2d490856321e78e39d6
mickdowling16 <michaeldowling16@gmail.com>
2024-04-15 12:17:59 +0000

    add content to homepage. Navbar and hero

--- marketingwebsite/settings.py
+++ marketingwebsite/settings.py
@@ -73,6 +73,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'marketingwebsite.wsgi.application'
 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -122,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
