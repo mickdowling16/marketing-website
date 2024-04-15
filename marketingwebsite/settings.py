a895167f22c9b2149b8eb3904388411068793d5d
mickdowling16 <michaeldowling16@gmail.com>
2024-04-15 15:00:06 +0000

    add services we offer and our parthners section on homepage

--- marketingwebsite/settings.py
+++ marketingwebsite/settings.py
@@ -128,6 +128,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
